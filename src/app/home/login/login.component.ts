c67975872550f1d9c577dade7839385408be05d7
Guilherme Reis <guigareisbr@gmail.com>
2022-08-19 16:17:40 -0300

    Criando funcionalidade de login

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        alert('Usuário ou senha inválidos');
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }

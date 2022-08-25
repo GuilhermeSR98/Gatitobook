8bb3de7249c46594167404fb54d6608851256971
Guilherme Reis <guigareisbr@gmail.com>
2022-08-25 10:48:06 -0300

    Desenvolvendo componente novo-usuario

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,8 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent,
+      },
+      {
+        path: 'NovoUsuario',
+        component: NovoUsuarioComponent,
       },
     ],
   },
